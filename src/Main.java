0df4c7438b92cfb859af02dfeb1ce7a8d1db7c6f
Johnny Kava <johnnylk2003@hotmail.com>
2025-05-29 10:56:14 -0300

    criando menu e do while

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        int escolha;
 
+        do{
+            escolha = menu();
+
+        }while(escolha != 5);
+
+
+    }
+
+    public static int menu(){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("---------- MENU ----------");
+        System.out.println("[1] Adicionar Tarefa");
+        System.out.println("[2] Remover Tarefa");
+        System.out.println("[3] Listar todas as tarefas");
+        System.out.println("[4] Buscar Tarefa");
+        System.out.println("[5] Sair");
+        System.out.print("Digite um número: ");
+        int escolha = scanner.nextInt();
+        scanner.nextLine();
+
+        return escolha;
     }
 }
